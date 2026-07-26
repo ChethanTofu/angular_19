893cd8dba413610d5e85363ef52793fd57aa0685
ChethanTofu <chethanlearning15ec403@gmail.com>
2026-07-26 18:04:20 +0530

    Update: String Interplolation

--- angular-19-learning/src/app/app.component.ts
+++ angular-19-learning/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, TestComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-19-learning';
+  title = 'angular-19 Application';
+
+  a = 5;
+  b = 10;
+
+  ans  = this.a + this.b;
+  
 }
